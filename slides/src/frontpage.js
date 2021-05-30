596cb08fa1e042237722dba489aee53aa7c7fa56
Raph <rsuttiyotin@seek.com.au>
2021-05-30 16:03:01 +1000

    Adjust frontpage

--- slides/src/frontpage.js
+++ slides/src/frontpage.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Frontpage = ({ children }) => (
   <div
     style={{
-      width: '75vw',
-      height: '33vw',
+      width: '100vw',
+      height: '60vw',
       backgroundColor: 'rgb(1, 22, 39)',
-      opacity: 0.85,
-      padding: 10,
+      opacity: 0.8,
+      padding: 250,
     }}
   >
     {children}

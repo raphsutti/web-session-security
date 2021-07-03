c6e64abfc2b5f6858ee66e0df48dac4931b77c2c
Raph <rsuttiyotin@seek.com.au>
2021-07-03 14:28:06 +1000

    Slides update

--- slides/src/frontpage.js
+++ slides/src/frontpage.js
@@ -3,11 +3,12 @@ import React from 'react'
 const Frontpage = ({ children }) => (
   <div
     style={{
-      width: '100vw',
-      height: '60vw',
+      // width: '100vw',
+      // height: '60vw',
       backgroundColor: 'rgb(1, 22, 39)',
       opacity: 0.8,
-      padding: 250,
+      padding: 20,
+      verticalAlign: "middle",
     }}
   >
     {children}
